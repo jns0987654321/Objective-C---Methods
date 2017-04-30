fdeda1b12f4dbd8457428eea67224aa69f7814fa
Junior Samaroo <jns0987654321@gmail.com>
2017-04-29 22:20:06 -0400

    Initial Commit

---
+++ Methods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Methods
+//
+//  Created by Junior Samaroo on 2017-04-29.
+//  Copyright © 2017 Junior Samaroo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Methods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Methods
+//
+//  Created by Junior Samaroo on 2017-04-29.
+//  Copyright © 2017 Junior Samaroo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
